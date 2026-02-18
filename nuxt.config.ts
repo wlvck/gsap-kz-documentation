1a4469c89d610d294c3c355c4792df7aca30c0a1
Kanagat Zhumagaliev <zhumagalievkanagat@gmail.com>
2026-02-18 12:05:42 +0700

    chore: add initial Tailwind CSS configuration file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/tailwindcss"],
+  css: ["~/assets/css/tailwind.css"],
+  tailwindcss: {},
+  ssr: false,
+});
